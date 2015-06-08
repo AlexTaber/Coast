a1bec6f97d3b890f883f5837ffa30fced215254b
AlexTaber <alex.taber0@gmail.com>
2015-06-08 19:59:48 -0400

    initiated circled based design

--- app/assets/javascripts/player_view.js
+++ app/assets/javascripts/player_view.js
@@ -3,23 +3,32 @@ var PlayerView = function(plyr) {
 }
 
 PlayerView.prototype.drawSquare = function() {
-  this.plyr.moves[0].x = this.plyr.x;
-  this.plyr.moves[0].y = this.plyr.y - 16;
-  this.plyr.moves[0].angle = 270
-  this.plyr.moves[0].tint = this.plyr.moves[0].color;
+  var q0 = this.plyr.moves[0];
+  var q1 = this.plyr.moves[1];
+  var q2 = this.plyr.moves[2];
+  var q3 = this.plyr.moves[3];
 
-  this.plyr.moves[1].x = this.plyr.x + 16;
-  this.plyr.moves[1].y = this.plyr.y;
-  this.plyr.moves[1].angle = 0
-  this.plyr.moves[1].tint = this.plyr.moves[1].color;
+  q0.clear();
+  q0.beginFill(q0.color)
+  q0.arc(this.plyr.x, this.plyr.y, 64, game.math.degToRad(225), game.math.degToRad(315), false);
+  q0.endFill();
+  q0.bringToFront();
 
-  this.plyr.moves[2].x = this.plyr.x;
-  this.plyr.moves[2].y = this.plyr.y + 16;
-  this.plyr.moves[2].angle = 90
-  this.plyr.moves[2].tint = this.plyr.moves[2].color;
+  q1.clear();
+  q1.beginFill(q1.color)
+  q1.arc(this.plyr.x, this.plyr.y, 64, game.math.degToRad(315), game.math.degToRad(45), false);
+  q1.endFill();
+  q1.bringToFront();
 
-  this.plyr.moves[3].x = this.plyr.x - 16;
-  this.plyr.moves[3].y = this.plyr.y;
-  this.plyr.moves[3].angle = 180
-  this.plyr.moves[3].tint = this.plyr.moves[3].color;
+  q2.clear();
+  q2.beginFill(q2.color)
+  q2.arc(this.plyr.x, this.plyr.y, 64, game.math.degToRad(45), game.math.degToRad(135), false);
+  q2.endFill();
+  q2.bringToFront();
+
+  q3.clear();
+  q3.beginFill(q3.color)
+  q3.arc(this.plyr.x, this.plyr.y, 64, game.math.degToRad(135), game.math.degToRad(225), false);
+  q3.endFill();
+  q3.bringToFront();
 }
